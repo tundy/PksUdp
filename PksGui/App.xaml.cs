00a889d62012c693155628b0d64f3c86535ad66a
Tundy Matúš Tundér <tunder.matus@gmail.com>
2016-10-28 17:54:57 +0200

    UnhandledException to file

--- PksGui/App.xaml.cs
+++ PksGui/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Security.Permissions;
 
 namespace PksGui
 {
@@ -13,5 +15,17 @@ namespace PksGui
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            if (ex == null) return;
+            File.WriteAllText($"{DateTime.Now:yyyy-MM-dd HH-mm-ss}.dmp", ex.ToString());
+
+        }
     }
 }
